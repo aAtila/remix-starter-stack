74ee298f1ccfe5afd40242506ef630f611b10f69
Atila Alacan <alacanatila@gmail.com>
2022-12-18 11:47:19 +0100

    Tailwind tweaks

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-import styles from "~/styles/main.css";
+import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
