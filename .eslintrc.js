2feca6492a899cfaebb3271c69bcbd4a1277a43b
Atila Alacan <alacanatila@gmail.com>
2022-12-18 11:55:41 +0100

    Eslint plugin for tailwind - `npm i -D eslint-plugin-tailwindcss`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node", "prettier"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+  ],
+  // uncomment in case we need to custom tweak the options
+  // "rules": {
+  //   "tailwindcss/classnames-order": "warn",
+  //   "tailwindcss/enforces-negative-arbitrary-values": "warn",
+  //   "tailwindcss/enforces-shorthand": "warn",
+  //   "tailwindcss/migration-from-tailwind-2": "warn",
+  //   "tailwindcss/no-arbitrary-value": "off",
+  //   "tailwindcss/no-custom-classname": "warn",
+  //   "tailwindcss/no-contradicting-classname": "error"
+  // }
 };
