46afbc3794bfed5f4f20a01991dde41af369057f
Atila Alacan <alacanatila@gmail.com>
2022-12-18 12:02:37 +0100

    Welcome text styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,7 @@
 export default function Index() {
-  return <h1 className='text-3xl font-bold underline'>Hello from the Remix Starter Stack!</h1>;
+  return (
+    <h1 className='mt-8 bg-gradient-to-r from-sky-400 to-sky-700 bg-clip-text text-center text-8xl font-extrabold text-transparent'>
+      Hello from the Remix Starter Stack!
+    </h1>
+  );
 }
